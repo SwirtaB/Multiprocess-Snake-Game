4006675a6347dc440031805f7cf24ade8ea0bd1c
SwirtaB <git@swirta.pl>
2021-05-07 21:48:23 +0200

    Minor fixes

--- src/Snake.cpp
+++ src/Snake.cpp
@@ -1,5 +1,5 @@
 //
-// Created by swirta on 02.05.2021.
+// Created pointB.y swirta on 02.05.2021.
 //
 
 #include "../include/Snake.hpp"
@@ -48,7 +48,7 @@ bool Snake::is_intersected(const cv::Point &a, const cv::Point &b, const cv::Poi
 bool Snake::check_snake() {
     if(snakeBody.size() > 3){
         Point a, b, c, d;
-        int snakeSize = snakeBody.size();
+        int snakeSize = snakeBody.size() - 1;
         a = get_point(0);
         b = get_point(1);
         for(int i = 3; i < snakeSize; ++i){
@@ -83,49 +83,56 @@ void Game::draw(Mat &frame){
     snake.draw(frame);
     circle(frame, this->fruit, fruitRadius, Scalar(255, 0, 0), FILLED);
     putText(frame, "LIVES: " + to_string(lives), Point(50, 50), FONT_HERSHEY_DUPLEX, 1, Scalar(0, 255, 0), 2);
-    putText(frame, "SCORE: " + to_string(points), Point(windowSize.x - 400, 50), FONT_HERSHEY_DUPLEX, 1, Scalar(0, 255, 0), 2);
+    putText(frame, "SCORE: " + to_string(points), Point(windowSize.x - 200, 50), FONT_HERSHEY_DUPLEX, 1, Scalar(0, 255, 0), 2);
 
 }
 
+void Game::draw_lost_message(cv::Mat &frame) {
+    putText(frame, "PRZEGRALES", Point(windowSize.x/2 - 500, windowSize.y/2), FONT_HERSHEY_DUPLEX, 5, Scalar(0, 0, 255), 3);
+    putText(frame, "Nacisnij ENTER by kontynuowac", Point(windowSize.x/2 - 300, windowSize.y/2 + 100), FONT_HERSHEY_DUPLEX, 1, Scalar(0, 255, 0), 2);
+}
+
+void Game::draw_info_message(cv::Mat &frame) {
+    putText(frame, "By rozpoczac od nowa kliknij R, by wyjsc kliknij Esc", Point(windowSize.x/2 - 400, windowSize.y/2), FONT_HERSHEY_DUPLEX, 1, Scalar(0, 255, 0), 2);
+}
+
 void Game::run(Point &marker, Mat &frame, bool *close) {
-    if(gameState == PREPARING){
-        create_snake(marker);
-        generate_fruit();
-        gameState = PLAYING;
-        return;
-    }
-    else if(gameState == PLAYING){
-        snake.move(marker);
-        if(snake.check_snake())
-            --lives;
-//        if(lives == 0)
-//            gameState = LOST;
-        if(snake.eat_fruit(fruit)){
-            ++points;
+    switch (gameState) {
+        case PREPARING:
+            create_snake(marker);
             generate_fruit();
-        }
-        this->draw(frame);
-        int key = waitKey(1);
-        if(key == 27){
-            gameState = LOST;
-        }
-        return;
-    }
-    else if(gameState == LOST){
-        putText(frame, "Przegrales", Point(windowSize.x/2, windowSize.y/2), FONT_HERSHEY_DUPLEX, 1, Scalar(0, 255, 0), 2);
-        waitKey(1);
-        gameState = ENDED;
-        return;
-    }
-    else if(gameState == ENDED){
-        lives = 0, points = 0;
-        snake.clear_snake();
-        putText(frame, "By rozpoczac od nowa kliknij R, by wyjsc kliknij Esc", Point(windowSize.x/2 - 200, windowSize.y/2), FONT_HERSHEY_DUPLEX, 1, Scalar(0, 255, 0), 2);
-        int key = waitKey(1);
-        if(key == 27)
-            *close = true;
-        else if(key == 114)
-            gameState = PREPARING;
-        return;
+            gameState = PLAYING;
+            break;
+        case PLAYING:
+            snake.move(marker);
+            if(snake.check_snake())
+                --lives;
+            if(lives == 0)
+                gameState = LOST;
+            if(snake.eat_fruit(fruit)){
+                ++points;
+                generate_fruit();
+            }
+            this->draw(frame);
+            if(waitKey(1) == 27){
+                gameState = ENDED;
+            }
+            break;
+        case LOST:
+            draw_lost_message(frame);
+            if(waitKey(1) == 13)
+                gameState = ENDED;
+            break;
+        case ENDED:
+            lives = 3, points = 0;
+            snake.clear_snake();
+            draw_info_message(frame);
+
+            int key = waitKey(1);
+            if(key == 27)
+                *close = true;
+            else if(key == 114)
+                gameState = PREPARING;
+            break;
     }
 }
